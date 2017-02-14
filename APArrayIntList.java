1eecbe82136a1f5f8e77ca099f1756d2cfc6846a
Alan Chu <ualch9@gmail.com>
2017-02-13 17:23:09 -0800

    Update

--- APArrayIntList.java
+++ APArrayIntList.java
@@ -1,29 +1,28 @@
 public class APArrayIntList implements APIntList {
     private int[] list;
     private int index;
-
+    
     public APArrayIntList() {
         this.list = new int[5];
-        this.index = 1;
+        this.index = 0;
     }
-
+    
     /**
      * Returns the size of the list
      */
     public int size() {
         return this.index;
     }
-
+    
     /**
      * Adds the item to the end of the list
      * @param item
      */
     public void add(int item) {
-        resizeIfNeeded();
+        resize();
         this.list[this.index - 1] = item;
-        index++;
     }
-
+    
     /**
      * Add item to the specified index, move everything else up
      * @param index
@@ -33,12 +32,11 @@ public class APArrayIntList implements APIntList {
         if (index < 0 || index >= this.size() + 1) {
             throw new IndexOutOfBoundsException();
         }
-        resizeIfNeeded();
+        resize();
         System.arraycopy(this.list, index, this.list, index + 1, this.index - index);
         this.list[index] = item;
-        index++;
     }
-
+    
     /**
      * Replaces element at index with item
      * @param index
@@ -48,13 +46,14 @@ public class APArrayIntList implements APIntList {
         if (index < 0 || index >= this.size()) {
             throw new IndexOutOfBoundsException();
         }
+        
         this.list[index] = item;
     }
-
+    
     /**
      * Gets item at specified index
      * @param index
-     * @return int at index
+     * @return
      */
     public int get(int index) {
         if (index < 0 || index >= this.size()) {
@@ -62,20 +61,21 @@ public class APArrayIntList implements APIntList {
         }
         return this.list[index];
     }
-
+    
     /**
      * Checks if item exists in list
      * @param item
      * @return If the item exists
      */
     public boolean contains(int item) {
-        for (int i = 0; i < this.list.length - 1; i++)
-            if (this.list[i] == item)
+        for (int i = 0; i < this.list.length - 1; i++) {
+            if (this.list[i] == item) {
                 return true;
-                
+            }
+        }
         return false;
     }
-
+    
     /**
      * Removes the index at the specified index
      * @param index
@@ -91,12 +91,11 @@ public class APArrayIntList implements APIntList {
             System.arraycopy(this.list, index + 1, this.list, index, moved);
         }
         this.list[--this.index] = 0;
-        
         return itemToMove;
     }
-
+    
     /**
-     * Nulls out all values and trims list down to 0
+     * Nil all values and trims list down to 0
      * <b>This method is destructive</b>
      */
     public void clear() {
@@ -104,21 +103,15 @@ public class APArrayIntList implements APIntList {
         this.index = 0;
     }
     
-    /**
-     * Resizes the underlying array if it's about to overflow.
-     */
-    private void resizeIfNeeded() {
-        if (this.size() + 1 > this.list.length) {
-            resize();
-        }
-    }
-    
-    /// Rebuild and increment list size
+    /// Rebuild and increment list size if needed
     private void resize() {
-        int[] newList = new int[this.list.length * 2];
-        for (int i = 0; i < this.list.length - 1; i++) {
-            newList[i] = this.list[i];
+        ++this.index;
+        if (this.size() + 1 > this.list.length) {
+            int[] newList = new int[this.list.length * 2];
+            for (int i = 0; i < this.list.length - 1; i++) {
+                newList[i] = this.list[i];
+            }
+            this.list = newList;
         }
-        this.list = newList;
     }
 }
